e27d9492506b22dfb635953d4194971dfe4ab109
HPNeo <hpneo@hotmail.com>
2014-09-27 15:45:36 -0500

    Crear APK

--- CheckinApp_ManejoArchivos/Resources/Resource.designer.cs
+++ CheckinApp_ManejoArchivos/Resources/Resource.designer.cs
@@ -24,6 +24,10 @@ namespace CheckinApp_ManejoArchivos
 			global::Android.Runtime.ResourceIdManager.UpdateIdValues();
 		}
 		
+		public static void UpdateIdValues()
+		{
+		}
+		
 		public partial class Attribute
 		{
 			
